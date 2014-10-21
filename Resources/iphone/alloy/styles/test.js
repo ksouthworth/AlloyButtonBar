3d2278bfa4622e20071a8a78b9a1e6a29a36133e
Kevin Southworth <kevin.southworth@webascender.com>
2014-10-20 23:28:44 -0400

    remove Resources

--- Resources/iphone/alloy/styles/test.js
+++
@@ -1,1 +0,0 @@
-module.exports = [{"isClass":true,"priority":10000.0007,"key":"barClass1","style":{backgroundColor:"purple",}},{"isClass":true,"priority":10000.0009,"key":"barClass2","style":{}},{"isClass":true,"priority":10000.001,"key":"barClass3","style":{backgroundColor:"orange",}}];
